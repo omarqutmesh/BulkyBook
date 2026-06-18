b4a035a3a15d87a55de947abe78dbff53969902c
Omar Emad <omarqutmesh@gmail.com>
2026-06-18 17:29:07 +0300

    Add a new clouumn and seed Data (DisplayOrder)

--- OmarQutmeshWeb/Models/Category.cs
+++ OmarQutmeshWeb/Models/Category.cs
@@ -7,5 +7,6 @@ namespace OmarQutmeshWeb.Models
         //[Key]
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public int DisplayOrder { get; set; }
     }
 }
