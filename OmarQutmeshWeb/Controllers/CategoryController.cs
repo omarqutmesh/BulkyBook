5fbb651dbebdb04dec9e4a3f9188526184a92602
Omar Emad <omarqutmesh@gmail.com>
2026-06-18 17:47:33 +0300

    Retrevie Categories

--- OmarQutmeshWeb/Controllers/CategoryController.cs
+++ OmarQutmeshWeb/Controllers/CategoryController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            var categories = _context.Categories.ToList();
             return View("Index");
         }
     }
