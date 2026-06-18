d5cba620c8ec59092852067dfc741642885f94cc
Omar Emad <omarqutmesh@gmail.com>
2026-06-18 17:00:29 +0300

    Section 3: Create Categort Contoller

---
+++ OmarQutmeshWeb/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
