eba7fc9c3cb12340d4e06ea0836ef6ada651c60c
Omar Emad <omarqutmesh@gmail.com>
2026-06-20 18:08:33 +0300

    Delete in Action

--- OmarQutmeshWeb/Controllers/CategoryController.cs
+++ OmarQutmeshWeb/Controllers/CategoryController.cs
@@ -71,6 +71,35 @@ namespace BulkyBookWeb.Controllers
             }
             return View();
         }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public IActionResult DeletePost(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            return View();
+        }
     }
 
 }
