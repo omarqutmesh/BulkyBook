a7b28780bee76603b860162c71dddb8d721047e9
Omar Emad <omarqutmesh@gmail.com>
2026-06-20 17:38:24 +0300

    Load Category on Update

--- OmarQutmeshWeb/Controllers/CategoryController.cs
+++ OmarQutmeshWeb/Controllers/CategoryController.cs
@@ -39,5 +39,38 @@ namespace BulkyBookWeb.Controllers
             }
             return View();
         }
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+            }
+
+            var category = _context.Categories.Find(id);
+            if (category == null) 
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Update")]
+        public IActionResult UpdatePost(Category category)
+        {
+            if (!String.IsNullOrEmpty(category.Name) && _context.Categories.Any(c => c.Name.ToLower() == category.Name.ToLower()))
+            {
+                ModelState.AddModelError("", "Category name already exists! ");
+            }
+            if (ModelState.IsValid)
+
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
+
 }
