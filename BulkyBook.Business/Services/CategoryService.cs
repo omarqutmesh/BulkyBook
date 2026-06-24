43df7bfdaa533b9d4f6f334917bcc177571a4363
Omar Emad <omarqutmesh@gmail.com>
2026-06-24 19:31:15 +0300

    Category Service Interface

---
+++ BulkyBook.Business/Services/CategoryService.cs
@@ -0,0 +1,36 @@
+﻿using BulkyBook.Business.Services.IServices;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Business.Services
+{
+    public class CategoryService : ICategorySerivce
+    {
+        public Task<Category> CreateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category?> GetCategoryByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BulkyBook.Business/Services/IServices/ICategorySerivce.cs
@@ -0,0 +1,17 @@
+﻿using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Business.Services.IServices
+{
+    public interface ICategorySerivce
+    {
+        Task<Category?> GetCategoryByIdAsync(int id);
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+
+        Task<Category> CreateCategoryAsync(Category category);
+        Task UpdateCategoryAsync(Category category);
+        Task DeleteCategoryAsync(int id);
+    }
+}
