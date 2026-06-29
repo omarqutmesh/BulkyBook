a91b6d4368c4832d2efb41a1fb7e4c404c6e63fa
Omar Emad <omarqutmesh@gmail.com>
2026-06-29 20:37:21 +0300

    Section 6 : Assignment Solution - Product Model

---
+++ BulkyBook.Models/Product.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.Extensions.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string ISBN { get; set; } = string.Empty;
+
+        [Required]
+        public string Author { get; set; } = string.Empty;
+
+        [Display(Name ="List Price")]
+        [Required]
+        [Range(1,1000)]
+        public double ListPrice { get; set; }
+
+        [Display(Name ="Price for 1-50")]
+        [Required]
+        [Range(1,1000)]
+        public double Price { get; set; }
+
+        [Display(Name ="Price for 50+")]
+        [Required]
+        [Range(1,1000)]
+        public double Price50 { get; set; }
+
+        [Display(Name = "Price for 100+")]
+        [Required]
+        [Range(1, 1000)]
+        public double Price100 { get; set; }
+
+        [ValidateNever]
+        [Display(Name ="Product Image")]  
+        public string? ImageUrl { get; set; }
+    }
+}
