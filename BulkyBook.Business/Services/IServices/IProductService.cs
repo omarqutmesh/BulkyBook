1b1b3e7f70aa03dfb6c3357c5cbb187af0539667
Omar Emad <omarqutmesh@gmail.com>
2026-06-30 15:34:16 +0300

    Section 6 : Assignment Solution Part 1 - Product CRUD

---
+++ BulkyBook.Business/Services/IServices/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Business.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<Product?> GetProductByIdAsync(int id);
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product> CreateProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+
+        Task<bool> IsProductNameUniqueAsync(string name, int? productId = null);
+    }
+}
---
+++ OmarQutmeshWeb/Areas/Customer/Controllers/ProductController.cs
@@ -0,0 +1,110 @@
+﻿using BulkyBook.Business.Services.IServices;
+using BulkyBook.Models;
+using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            return View(products);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Create")]
+        public async Task<IActionResult> CreatePOST(Product product)
+        {
+            if (!String.IsNullOrEmpty(product.Name) && !await _productService.IsProductNameUniqueAsync(product.Name))
+            {
+                ModelState.AddModelError("", "Product name already exists!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _productService.CreateProductAsync(product);
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var product = await _productService.GetProductByIdAsync(id.Value);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Update")]
+        public async Task<IActionResult> UpdatePOST(Product product)
+        {
+            if (!String.IsNullOrEmpty(product.Name) &&
+                !await _productService.IsProductNameUniqueAsync(product.Name, product.Id))
+            {
+                ModelState.AddModelError("", "Product name already exists!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _productService.UpdateProductAsync(product);
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var product = await _productService.GetProductByIdAsync(id.Value);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeletePOST(int id)
+        {
+            await _productService.DeleteProductAsync(id);
+            TempData["success"] = "Product deleted successfully";
+            return RedirectToAction("Index");
+
+        }
+    }
+}
