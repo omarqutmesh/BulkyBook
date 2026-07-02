080e568d2b8c0542dc1f434c3d1aac8baed117c5
Omar Emad <omarqutmesh@gmail.com>
2026-07-02 18:14:35 +0300

    Section 6 : Customizing DataTables

--- OmarQutmeshWeb/wwwroot/js/products.js
+++ OmarQutmeshWeb/wwwroot/js/products.js
@@ -1,11 +1,28 @@
 ﻿$('#tblData').DataTable({
     ajax: '/product/getall',
     columns: [
-        { data: "title" },
-        { data: "isbn" },
-        { data: "price" },
-        { data: "author" },
-        { data: 'category.name' },
-        { defaultContent: '' },
+        { data: "title" ,width : "25%" },
+        { data: "isbn", width: "15%" },
+        { data: "price", width: "10%", "render": function (data) { return '$' + data.toFixed(2); } },
+        { data: "author", width: "15%" },
+        {
+            data: 'category.name', width: "10%", "render": function (data) {
+                return '<span class = "badge bg-secondary">' + data + '</span>'
+            }
+        },
+        {
+            data: 'id', width: "25%", "render": function (data)
+            {
+                return `<div class = "d-flex gap-2 justify-content-end">
+                 <a href="/product/edit?id=${data}" class="btn btn-sm btn-outline-success">
+                                 <i class="bi bi-pencil-square"></i> Edit
+                            </a>
+                              <a href="/product/edit?id=${data}" class="btn btn-sm btn-outline-danger">
+                                 <i class="bi bi-trash"></i> Delete
+                            </a>
+            </div>`
+            }
+            
+},
     ]
 });
