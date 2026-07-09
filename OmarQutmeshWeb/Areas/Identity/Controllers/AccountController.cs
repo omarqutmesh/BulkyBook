a30da510ee10c6ad8da99a22def9b46e699dde51
Omar Emad <omarqutmesh@gmail.com>
2026-07-09 16:03:55 +0300

    Section 8 : Login Register and AccessDenied Templates

---
+++ OmarQutmeshWeb/Areas/Identity/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Identity.Controllers
+{
+    [Area("Identity")]
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
