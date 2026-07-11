ebb4f2eca4b7e80d8280a5c0f3c712e3dab6adb0
Omar Emad <omarqutmesh@gmail.com>
2026-07-11 17:50:47 +0300

    Section 8 : Bind RegisterVM

---
+++ BulkyBook.Models/ViewModels/RegisterVM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BulkyBook.Models.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Display(Name = "Street Address")]
+        public string? StreetAddress { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+
+        [Display(Name = "Postal Code")]
+        public string? PostalCode { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string? PhoneNumber { get; set; }
+    }
+}
