e773a58241ef9ae2762dca86fcf666957dd4a4f5
Omar Emad <omarqutmesh@gmail.com>
2026-07-18 14:22:56 +0300

    Section 10 : Shopping Cart Model

---
+++ BulkyBook.Models/ShoppingCart.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+        public int Count { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        [NotMapped]
+        public double Price
+        {
+            get
+            {
+                if (Product == null) return 0;
+
+                if (Count < 50)
+                {
+                    return Product.Price;
+                }
+                else if (Count < 100)
+                {
+                    return Product.Price50;
+                }
+                else
+                {
+                    return Product.Price100;
+                }
+            }
+        }
+    }
+}
