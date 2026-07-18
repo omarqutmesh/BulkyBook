ac43eab10d529901edebaf96add732e0b148c953
Omar Emad <omarqutmesh@gmail.com>
2026-07-18 14:26:38 +0300

    Section 10 : Order Header Model

---
+++ BulkyBook.Models/OrderHeader.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    public class OrderHeader
+    {
+        public int Id { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public DateTime ShippingDate { get; set; }
+
+        public double OrderTotal { get; set; }
+
+        public string? OrderStatus { get; set; }
+
+        public string? TrackingNumber { get; set; }
+        public string? Carrier { get; set; }
+
+
+        public string? SessionId { get; set; }
+        public string? PaymentIntentId { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Name { get; set; }
+    }
+}
