49cb5ff082d32bdbe0f7392c1f8ff9a3f2a88584
Omar Emad <omarqutmesh@gmail.com>
2026-07-19 14:03:32 +0300

    Section 10 : ShoppingCartService Interface

---
+++ BulkyBook.Business/Services/IServices/IShoppingCartService.cs
@@ -0,0 +1,18 @@
+﻿using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Business.Services.IServices
+{
+    public class IShoppingCartService
+    {
+        Task<ShoppingCart?> GetCartByIdAsync(int cartId);
+        Task<IEnumerable<ShoppingCart>> GetUserCartItemsAsync(string userId);
+        Task<int> GetCartCountAsync(string userId);
+        Task<ShoppingCart> AddToCartAsync(ShoppingCart cart);
+        Task UpdateCartAsync(ShoppingCart cart);
+        Task ClearCartAsync(string userId);
+
+    }
+}
