497d0043dfa644ca2578453fdb7a68c12de7f0fe
Omar Emad <omarqutmesh@gmail.com>
2026-07-19 14:30:26 +0300

    Section 10 : Implement Cart Service Part 2

--- BulkyBook.Business/Services/ShoppingCartService.cs
+++ BulkyBook.Business/Services/ShoppingCartService.cs
@@ -46,14 +46,36 @@ namespace BulkyBook.Business.Services
                 .ToListAsync();
         }
 
-        public Task<ShoppingCart> AddToCartAsync(ShoppingCart cart)
+        public async Task<ShoppingCart> AddToCartAsync(ShoppingCart cart)
         {
-            throw new NotImplementedException();
+            var existingItem = await _context.ShoppingCarts.Include(u => u.Product)
+                .FirstOrDefaultAsync(u => u.ApplicationUserId == cart.ApplicationUserId && u.ProductId == cart.ProductId);
+
+            if (existingItem != null)
+            {
+                existingItem.Count += cart.Count;
+                await _context.SaveChangesAsync();
+                return existingItem;
+            }
+            else
+            {
+                _context.ShoppingCarts.Add(cart);
+                await _context.SaveChangesAsync();
+                return cart;
+            }
         }
 
-        public Task UpdateCartAsync(ShoppingCart cart)
+        public async Task UpdateCartAsync(ShoppingCart cart)
         {
-            throw new NotImplementedException();
+            if (cart.Count <= 0)
+            {
+                _context.ShoppingCarts.Remove(cart);
+            }
+            else
+            {
+                _context.ShoppingCarts.Update(cart);
+            }
+            await _context.SaveChangesAsync();
         }
     }
 }
