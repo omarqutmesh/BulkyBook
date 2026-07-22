8330f48e92c80340f2e9b1a01aa540606c6a18c1
Omar Emad <omarqutmesh@gmail.com>
2026-07-22 19:28:15 +0300

    Section 10 : Cart UI

---
+++ OmarQutmeshWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,28 @@
+﻿using BulkyBook.Business.Services.IServices;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+
+
+namespace BulkyBookWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly IShoppingCartService _shoppingCartService;
+
+        public CartController(IProductService productService, IShoppingCartService shoppingCartService) 
+        {
+            _productService = productService;
+            _shoppingCartService = shoppingCartService;
+        }
+        public async Task<IActionResult> Index()
+        {
+                return View();
+        }
+  
+    }
+}
