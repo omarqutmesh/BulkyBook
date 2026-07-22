8bb7f3c8942dca65c3cfe3a283a3bd7364bf5eb3
Omar Emad <omarqutmesh@gmail.com>
2026-07-22 19:19:03 +0300

    Section 10 : Add items to Cart

--- OmarQutmeshWeb/Areas/Customer/Controllers/HomeController.cs
+++ OmarQutmeshWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using BulkyBook.Business.Services.IServices;
 using BulkyBook.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
+
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
@@ -9,10 +12,12 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly IProductService _productService;
+        private readonly IShoppingCartService _shoppingCartService;
 
-        public HomeController(IProductService productService) 
+        public HomeController(IProductService productService, IShoppingCartService shoppingCartService) 
         {
             _productService = productService;
+            _shoppingCartService = shoppingCartService;
         }
         public async Task<IActionResult> Index()
         {
@@ -33,11 +38,24 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
                 Count = 1,
                 ProductId = productId
             };
-            return View(product);
+            return View(cart);
         }
-        public IActionResult Privacy()
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Details(ShoppingCart shoppingCart)
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Unauthorized();
+            }
+
+            shoppingCart.ApplicationUserId = userId;
+            await _shoppingCartService.AddToCartAsync(shoppingCart);
+
+            return RedirectToAction("Details", new { productId = shoppingCart.ProductId });
         }
 
 
--- OmarQutmeshWeb/Program.cs
+++ OmarQutmeshWeb/Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 }).AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/identity/account/login";
