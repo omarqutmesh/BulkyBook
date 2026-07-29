a954d37804d80364705b3c769dc4cd8bf4b709f3
Omar Emad <omarqutmesh@gmail.com>
2026-07-29 17:59:04 +0300

    Section 12 : Order Controller

---
+++ OmarQutmeshWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,40 @@
+﻿using BulkyBook.Business.Services;
+using BulkyBook.Business.Services.IServices;
+using BulkyBook.DataAccess.Data;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utiltiy;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.RoleAdmin)]
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+
+            return View();
+        }
+
+        #region API CALLS
+        [AllowAnonymous]
+        public async Task<IActionResult> GetAll()
+        {
+            var orders = await _orderService.GetAllOrderAsync();
+            return Json(new { data = orders });
+        }
+
+        #endregion
+    }
+}
