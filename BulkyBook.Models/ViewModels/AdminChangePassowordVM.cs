c8e5782d452e09463b3284514f58d3d2ffe21771
Omar Emad <omarqutmesh@gmail.com>
2026-08-04 20:18:04 +0300

    Section 13 : Assignment Overview

---
+++ BulkyBook.Models/ViewModels/AdminChangePassowordVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BulkyBook.Models.ViewModels
+{
+    public class AdminChangePassowordVM
+    {
+        public string UserId { get; set; }
+
+        [Display(Name = "User Email")]
+        public string UserEmail { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        [Compare("NewPassword", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ BulkyBook.Models/ViewModels/RoleManagmentVM.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Models.ViewModels
+{
+    public class RoleManagmentVM
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<SelectListItem> RoleList { get; set; }
+    }
+}
