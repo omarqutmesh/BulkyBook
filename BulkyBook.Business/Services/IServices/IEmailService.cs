998cf346e3aa521b17271cd5ea78f62ec15e8d03
Omar Emad <omarqutmesh@gmail.com>
2026-08-11 13:06:18 +0300

    Section 14 : IEmailSender Interface

---
+++ BulkyBook.Business/Services/IServices/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Business.Services.IServices
+{
+    public interface IEmailService
+    {
+
+        Task<bool> SendEmailAsync(string toEmail, string subject, string htmlContent);
+
+        Task<bool> SendOrderConfirmationEmailAsync(string toEmail, int orderId, decimal orderTotal);
+    }
+}
