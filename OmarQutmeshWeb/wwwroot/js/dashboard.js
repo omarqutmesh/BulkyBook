de4eaa2ddacbdc914d5aad8bc90ad03d886a755e
Omar Emad <omarqutmesh@gmail.com>
2026-08-12 21:04:24 +0300

    Section 15 : Dynamic Data in Charts

---
+++ OmarQutmeshWeb/wwwroot/js/dashboard.js
@@ -0,0 +1,20 @@
+﻿$.getJSON('/Admin/Dashboard/GetChartData', function (data) {
+	new Chart(document.getElementById('revenueChart'), {
+		type: 'line',
+		data: {
+			labels: data.monthlyRevenue.map(r => r.label),
+			datasets: [{
+				label: 'Revenue',
+				data: data.monthlyRevenue.map(r => r.revenue),
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				y: {
+					beginAtZero: true
+				}
+			}
+		}
+	});
+})
