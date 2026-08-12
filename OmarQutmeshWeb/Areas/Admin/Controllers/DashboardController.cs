f80903b4a3a8bff082d857e5a827c73d6a4a187a
Omar Emad <omarqutmesh@gmail.com>
2026-08-12 17:53:42 +0300

    Section 15 : Endpoint for Revenue

--- OmarQutmeshWeb/Areas/Admin/Controllers/DashboardController.cs
+++ OmarQutmeshWeb/Areas/Admin/Controllers/DashboardController.cs
@@ -33,5 +33,30 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             };
             return View(dashboardVM);
         }
+        [HttpGet]
+        public async Task<IActionResult> GetChartData()
+        {
+            var orders = await _db.OrderHeaders.ToListAsync();
+            var products = await _db.Products.Include(u => u.Category).ToListAsync();
+            var categories = await _db.Categories.ToListAsync();
+
+
+            //Revenue by month(last 6 months)
+            var now = DateTime.UtcNow;
+            var sixMonthsAgo = now.AddMonths(-5);
+            var monthlyRevenue = Enumerable.Range(0, 6).Select(i =>
+            {
+                var month = sixMonthsAgo.AddMonths(i);
+                var revenue = orders.Where(o => o.OrderDate.Year == month.Year && o.OrderDate.Month == month.Month
+                && (o.OrderStatus == SD.StatusApproved || o.OrderStatus == SD.StatusShipped)).Sum(o => o.OrderTotal);
+
+                return new { Label = month.ToString("MMM yyyy"), Revenue = revenue };
+            }).ToList();
+
+            return Json(new
+            {
+                monthlyRevenue,
+            });
+        }
     }
 }
