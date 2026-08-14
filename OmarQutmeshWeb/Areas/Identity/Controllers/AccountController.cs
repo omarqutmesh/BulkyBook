0dd0f50216537eb8a59335f8b881fcc38c630d12
Omar Emad <omarqutmesh@gmail.com>
2026-08-14 11:06:45 +0300

    Section 17 : Disable Creating Admin Account

--- OmarQutmeshWeb/Areas/Identity/Controllers/AccountController.cs
+++ OmarQutmeshWeb/Areas/Identity/Controllers/AccountController.cs
@@ -59,15 +59,21 @@ namespace BulkyBookWeb.Areas.Identity.Controllers
 
         public IActionResult Register(string? returnUrl = null)
         {
-            var model = new RegisterVM
+            var model = new RegisterVM();
+            if (User.IsInRole(SD.RoleAdmin))
             {
-                RoleList =
-               [
-                   new SelectListItem{Text=SD.RoleCustomer, Value=SD.RoleCustomer},
+                model.RoleList =
+                    [
+                        new SelectListItem{Text=SD.RoleCustomer, Value=SD.RoleCustomer},
                     new SelectListItem{Text=SD.RoleAdmin, Value=SD.RoleAdmin},
                     new SelectListItem{Text=SD.RoleEmployee, Value=SD.RoleEmployee},
-                ]
-            };
+                ];
+            }
+            else
+            {
+                model.RoleList = new List<SelectListItem>();
+            }
+
             ViewData["ReturnUrl"] = returnUrl;
             return View(model);
         }
@@ -102,9 +108,11 @@ namespace BulkyBookWeb.Areas.Identity.Controllers
 
                 if (result.Succeeded)
                 {
-                    if (!string.IsNullOrEmpty(registerVM.Role))
+                    if (User.IsInRole(SD.RoleAdmin) && !string.IsNullOrEmpty(registerVM.Role))
                     {
                         await _userManager.AddToRoleAsync(user, registerVM.Role);
+                        TempData["success"] = $"User '{user.Name}' created successfully with role '{registerVM.Role}'.";
+                        return RedirectToAction("Index", "User", new { area = "Admin" });
                     }
                     else
                     {
@@ -112,7 +120,6 @@ namespace BulkyBookWeb.Areas.Identity.Controllers
                     }
                     //user has been created
                     await _signInManager.SignInAsync(user, isPersistent: false);
-                    // Redirect to returnUrl if valid, otherwise go to Home
                     if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
                     {
                         return Redirect(returnUrl);
